ba9d579bd5a6a789d4cfb0709e4fe610de07bf26
leo7chen <31997863+leo7chen@users.noreply.github.com>
2018-08-24 17:10:16 +0800

    leo

--- signup/trips/admin.py
+++ signup/trips/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import visitor
+admin.site.register(visitor)
 # Register your models here.
--- signup/trips/models.py
+++ signup/trips/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class visitor(models.Model):
-    name = models.TextField(max_length = 50, blank = True)
-    company = models.TextField(max_length = 50, blank = True)
-    visit_area = models.TextField(max_length = 50, blank = True)
-    purpose = models.TextField(max_length = 50, blank = True)
-    login_time = models.DateTimeField(auto_now_add=True)
-    logout_time = models.DateTimeField(auto_now_add=True)
+    name = models.TextField(max_length = 200)
+    company = models.TextField(max_length = 200)
+    visit_area = models.TextField(max_length = 200)
+    purpose = models.TextField(max_length = 200)
+    login_time = models.DateTimeField('Enter time')
+    logout_time = models.DateTimeField('Exit time')
     def __str__(self):
         return self.name
