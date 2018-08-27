451e83454e7a5984658fa70f765f826359a255b6
leo7chen <31997863+leo7chen@users.noreply.github.com>
2018-08-27 15:15:25 +0800

    leoo

--- signup/trips/views.py
+++ signup/trips/views.py
@@ -19,3 +19,8 @@ def new(request):
 		new.save()
 		# return redirect('new')
 	return render(request, 'trips/enter.html',{})
+
+def getImg(request):
+	file_content = ContentFile(request.FILES['img'].read())  
+    img = ImageStore(name = request.FILES['img'].name, img = request.FILES['img'])  
+    img.save()
