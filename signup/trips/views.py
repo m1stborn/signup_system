e019fe8d7f989deb27182dcef5e601837d62f726
施宗佑 <robin@shizongyoude-MacBook-Air.local>
2018-08-27 14:44:47 +0800

    aa

--- signup/trips/views.py
+++ signup/trips/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from .models import visitor
+from datetime import datetime
 # Create your views here.
 
 def hello_world(request):
 	return HttpResponse("Hello world!")
+
+
+
+def new(request):
+	if 'Name' and 'Company' and 'Purpose' in request.GET:
+		print("in")
+		Name = request.GET['Name']
+		Company = request.GET['Company']
+		Purpose = request.GET['Purpose']
+		new = visitor(name=Name, company=Company, purpose=Purpose)
+		new.save()
+		# return redirect('new')
+	return render(request, 'trips/enter.html',{})
