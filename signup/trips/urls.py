b98f6367f222d14dc27d8bfe07c90cc1704f5744
b04705031 <b04705031@ntu.edu.tw>
2018-10-31 14:12:29 +0800

    model_choice

--- signup/trips/urls.py
+++ signup/trips/urls.py
@@ -1,11 +1,12 @@
 #_*_coding:utf8_*_
 from django.urls import path
-from .views import login, logout, home, checkID, addID , query
+from .views import login, logout, home, checkID, addID , query, confirm
 urlpatterns = [
 	path('', home, name='home'),
     path('login', login, name='login'),
     path('logout', logout, name='logout'),
     path('checkID',checkID, name='checkID'),
     path('addID', addID, name='addID'),
-    path('query', query, name='query')
+    path('query', query, name='query'),
+    path('confirm', confirm, name="confirm")
 ]
